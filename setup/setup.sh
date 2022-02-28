fd01eea9c0de90d1c3c4af304428af0f7a5a87e0
MiriamNM <miriamnavamucino@gmail.com>
2022-02-28 15:14:18 -0600

    all the proyect

---
+++ setup/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd /setup
+
+# Wait just in case LocalStack delays the start
+sleep 7s
+
+# Create table in DynamoDB
+aws dynamodb create-table --endpoint-url http://localstack:4566 --cli-input-json file://Create-players.json
+
+# Insert data
+aws dynamodb batch-write-item --request-items file://players-v1.json --endpoint-url http://localstack:4566
+aws dynamodb batch-write-item --request-items file://players-v2.json --endpoint-url http://localstack:4566
+aws dynamodb batch-write-item --request-items file://players-v3.json --endpoint-url http://localstack:4566
+
